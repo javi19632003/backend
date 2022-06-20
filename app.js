474ace432b1df3c4cb1338a603e9251dc6b6e539
javi19632003 <anicolas@transclor.com.ar>
2022-06-20 19:10:41 -0300

    Ejercicio 4 clase 8

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 
-const express    = require('express');
-const router     = require('./routes.js')
-//const Contenedor = require('./clases.js');
-//const c1         = new Contenedor("productos.json");
-const app        = express()
+const express  = require('express');
+const router   = require('./routes.js')
+const app      = express()
+const PORT     = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', express.static(__dirname + "/public"));
 
-/*
-app.get('/productos', async function  (req, res) {
-  let resultado = await c1.getAll();
-    res.json(resultado)
-  })
-  
-  app.get('/productoRandom/:id', async (req, res) => {
-    console.log(req.params.id)
-    let resultado = await c1.getById(Number(req.params.id));
-    res.json(resultado)
-  })
-*/
-const PORT = 8080;
+
 app.use('/api', router)
+
 app.listen(PORT, () =>{
-    console.log(`Servidor Http escuchando en el puerto 8080`)
+    console.log(`Servidor Http escuchando en el puerto ${PORT}`)
 })
